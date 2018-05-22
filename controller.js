bd767468082fca3a9b538efac12c4578c76cdba0
Charity Cardinal <charcard@umich.edu>
2018-05-22 08:49:55 -0400

    First commit

--- controller.js
+++ controller.js
@@ -14,7 +14,20 @@ function toDoController() {
              complete: false
             }
     ]
-}
+
+    vm.addTask = (chore, complete) => {
+        vm.tasks.push({
+            chore: chore,
+            complete: false
+        });
+    }
+    vm.removeTask = (index) => {
+        vm.tasks.splice(index, 1);
+    }
+    vm.completeTask =(index) => {
+        vm.tasks[index].complete = true;
+    }
+};
 angular
     .module("toDoApp")
     .controller("toDoController", toDoController);
